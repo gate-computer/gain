d0d53981f138b9aafc854fc0d7bc99e10075ccfb
Timo Savola <timo.savola@iki.fi>
2021-03-02 20:36:53 +0200

    gain/service: clarify stream instantiation condition

--- gain/src/service.rs
+++ gain/src/service.rs
@@ -24,8 +24,8 @@ pub enum RegistrationError {
 /// Handle to a registered service.
 ///
 /// If a stream is opened as a result of service registration or a call, the
-/// appropriate stream constructor must be called immediately.  Consequently, a
-/// buffered stream cannot be used to receive data which includes stream ids.
+/// appropriate stream constructor must be called immediately.  Care must be
+/// taken when using input buffering with streams which carry stream ids.
 pub struct Service {
     code: Code,
 }
