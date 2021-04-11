4e5254edb1f03b81d16084dd724f024c67010842
Timo Savola <timo.savola@iki.fi>
2021-04-11 04:03:31 +0300

    examples: echo

---
+++ examples/echo.rs
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Timo Savola. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use gain::origin;
+use gain::stream::buf::{Read, ReadWriteStream, DEFAULT_READ_CAPACITY};
+use gain::stream::Write;
+use gain::task::block_on;
+use std::process::exit;
+
+fn main() {
+    exit(block_on(async {
+        let mut c = ReadWriteStream::new(origin::accept().await.unwrap());
+        let mut b = [0; DEFAULT_READ_CAPACITY];
+
+        loop {
+            let n = match c.read(&mut b[..]).await {
+                Ok(n) => n,
+                Err(_) => return 0,
+            };
+
+            match c.write_all(&b[..n]).await {
+                Ok(_) => {}
+                Err(_) => return 1,
+            }
+        }
+    }))
+}
